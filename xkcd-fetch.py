a188544956b7cfc64bf6cadfcad0bca237b3ef7f
Dennis Heidsiek <HeidsiekB@aol.com>
2011-09-07 03:37:15 +0200

    Store all parsed metadata in a list of dictionaries.

--- xkcd-fetch.py
+++ xkcd-fetch.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
+import html.parser, json, os, re
+import httplib2
 
-import re, httplib2
 
-http = httplib2.Http(".cache")
+def html_unescape(s):
+	p = html.parser.HTMLParser()
+	return p.unescape(s)
+
+
+http = httplib2.Http('.cache')
 
 def fetch(url):
 	response, content = http.request(url, 'GET')
@@ -12,19 +18,49 @@ def fetch(url):
 def fetch_text(url):
 	return fetch(url).decode('utf-8')
 
+
+if not os.path.exists('image'):
+	os.mkdir('image')
+
 archive = fetch_text('https://www.xkcd.com/archive/')
-pattern = r'href="/(\d+)/" title="(.*?)">(.*?)</a>'
-img_pattern = r'<img src="http://imgs.xkcd.com/comics/(.*?)" title="(.*?)" alt="(.*?)" /><br/>'
 
-for m in re.finditer(pattern, archive):
-	print(m.groups())
+archive_pattern = r'<a href="/(\d+)/" title="(\d{4}-\d{1,2}-\d{1,2})">(.*?)</a>'
+
+page_pattern = r'<img src="http://imgs.xkcd.com/comics/(.*?)" title="(.*?)" alt="(.*?)" /><br/>'
+
+
+index = []
+for a in re.finditer(archive_pattern, archive):
+	entry = dict()
+	entry['number']= a.group(1) # 886
+	entry['date']= a.group(2)   # 2011-4-15
+	entry['title']= a.group(3)  # Craigslist Apartments
+	print(entry)
 	
-	page = fetch_text('https://www.xkcd.com/'+m.group(1)+'/')
-	for c in re.finditer(img_pattern, page):
-		#print(c.groups())
-		o = "image/{:04d} {}".format(int(m.group(1)), c.group(1))
+	page = fetch_text('https://www.xkcd.com/'+a.group(1)+'/')
+	
+	for p in re.finditer(page_pattern, page):
+		
+		entry['file'] = p.group(1)                  # craigslist_apartments.png
+		entry['desc'] = html_unescape(p.group(2))   # $1600 / 1386153BR 3BATH, …
+		#entry['title'] = p.group(3)                # Craigslist Apartments
+		
+		o = 'image/{:04d}_{}'.format(int(entry['number']), entry['file'])
+		
+		for e in entry:
+			print('{:10s} = {}'.format(e, entry[e]))
+		print('Filename:', o)
+		
+		index.append(entry)
+		
+		image = open(o, 'wb')
+		image.write(fetch('http://imgs.xkcd.com/comics/'+entry['file']))
+		image.close()
+		
+		
 
-		file = open(o, "wb")
-		file.write(fetch("http://imgs.xkcd.com/comics/"+c.group(1)))
-		file.close()
+print(index)
+f = open('image/index.json', 'w')
+f.write(json.dumps(index, ensure_ascii=False, sort_keys=True, indent=2))
+f.close()
 
