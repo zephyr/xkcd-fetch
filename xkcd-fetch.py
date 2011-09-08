25bee1766c50d33cab55845ae81d9b093f256647
Dennis Heidsiek <HeidsiekB@aol.com>
2011-09-08 04:20:49 +0200

    Moved code into functions and added csv export.

--- xkcd-fetch.py
+++ xkcd-fetch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import html.parser, json, os, re
+import csv, html.parser, json, os, re
 import httplib2
 
 
@@ -19,48 +19,89 @@ def fetch_text(url):
 	return fetch(url).decode('utf-8')
 
 
+def read_index():
+	''' reads the index from a json file '''
+	f = open('image/index.json', 'r')
+	index = json.loads(f.read())
+	f.close()
+	return index
+
+def write_index(index):
+	''' Write the index to disk as json and csv files '''
+	
+	index.sort(key=lambda entry: int(entry['number']))
+	
+	f = open('image/index.json', 'w')
+	f.write(json.dumps(index, ensure_ascii=False, sort_keys=True, indent=2))
+	f.close()
+	
+	f = open('image/index.csv', 'w')	
+	writer = csv.DictWriter(f, ['number', 'date','title','desc','file'])#, quoting=csv.QUOTE_ALL)
+	#writer.writeheader() # will arrive in Python 3.2!
+	for entry in index:
+		writer.writerow(entry)
+	f.close()
+
+
+def fetch_index(index):
+	
+	archive = fetch_text('https://www.xkcd.com/archive/')
+	
+	archive_pattern = r'<a href="/(\d+)/" title="(\d{4}-\d{1,2}-\d{1,2})">(.*?)</a>'
+	
+	page_pattern = r'<img src="http://imgs.xkcd.com/comics/(.*?)" title="(.*?)" alt="(.*?)" /><br/>'
+	
+	for a in re.finditer(archive_pattern, archive):
+		entry = dict()
+		entry['number']= a.group(1) # 886
+		entry['date']= a.group(2)   # 2011-4-15
+		entry['title']= a.group(3)  # Craigslist Apartments
+		print(entry)
+		
+		page = fetch_text('https://www.xkcd.com/'+a.group(1)+'/')
+		
+		for p in re.finditer(page_pattern, page):
+			
+			entry['file'] = p.group(1)                  # craigslist_apartments.png
+			entry['desc'] = html_unescape(p.group(2))   # $1600 / 1386153BR 3BATH, …
+			#entry['title'] = p.group(3)                # Craigslist Apartments
+			
+			o = 'image/{:04d}_{}'.format(int(entry['number']), entry['file'])
+			
+			for e in entry:
+				print('{:10s} = {}'.format(e, entry[e]))
+			print('Filename:', o)
+			
+			index.append(entry)
+			
+			image = open(o, 'wb')
+			image.write(fetch('http://imgs.xkcd.com/comics/'+entry['file']))
+			image.close()
+			
+		
+	return index;
+
+
+
 if not os.path.exists('image'):
 	os.mkdir('image')
 
-archive = fetch_text('https://www.xkcd.com/archive/')
+try:
+	index = read_index() # from disk
+except:
+	index = fetch_index([])
 
-archive_pattern = r'<a href="/(\d+)/" title="(\d{4}-\d{1,2}-\d{1,2})">(.*?)</a>'
 
-page_pattern = r'<img src="http://imgs.xkcd.com/comics/(.*?)" title="(.*?)" alt="(.*?)" /><br/>'
+# Evaluate the index
+
+found = sorted([int(entry['number']) for entry in index])
+missing = [i for i in range(1, max(found)+1) if (i not in found) and (i != 404)]
+
+print('Found {:d} comics.'.format(len(found)))
+if len(missing)>0:
+	print('{:d} comics are missing:\n{}'.format(len(missing), missing))
 
 
-index = []
-for a in re.finditer(archive_pattern, archive):
-	entry = dict()
-	entry['number']= a.group(1) # 886
-	entry['date']= a.group(2)   # 2011-4-15
-	entry['title']= a.group(3)  # Craigslist Apartments
-	print(entry)
-	
-	page = fetch_text('https://www.xkcd.com/'+a.group(1)+'/')
-	
-	for p in re.finditer(page_pattern, page):
-		
-		entry['file'] = p.group(1)                  # craigslist_apartments.png
-		entry['desc'] = html_unescape(p.group(2))   # $1600 / 1386153BR 3BATH, …
-		#entry['title'] = p.group(3)                # Craigslist Apartments
-		
-		o = 'image/{:04d}_{}'.format(int(entry['number']), entry['file'])
-		
-		for e in entry:
-			print('{:10s} = {}'.format(e, entry[e]))
-		print('Filename:', o)
-		
-		index.append(entry)
-		
-		image = open(o, 'wb')
-		image.write(fetch('http://imgs.xkcd.com/comics/'+entry['file']))
-		image.close()
-		
-		
-
-print(index)
-f = open('image/index.json', 'w')
-f.write(json.dumps(index, ensure_ascii=False, sort_keys=True, indent=2))
-f.close()
+# Finally …
+write_index(index)
 
